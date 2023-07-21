cfefb563492c2480e36e9126e35e9c9009878df9
Dharmesh Bhatt <bhattd@bhattd-a01.vmware.com>
2023-07-21 10:13:57 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-if [ -z $DBENCH_MOUNTPOINT ]; then
-    DBENCH_MOUNTPOINT=/tmp
-fi
-
-
-if [ "$1" = 'fio' ]; then
-    fio --server --daemonize=/tmp/fio.pid
-    rm $DBENCH_MOUNTPOINT/fiotest
-    exit 0
-fi
-
-exec "$@"
+fio --server --daemonize=/tmp/fio.pid
